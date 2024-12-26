90cd416435887d1b05474e5b323f4e05859f55a2
GROOT-CPU <2k23it53@kiot.ac.in>
2024-12-26 16:05:30 +0530

    Add files via upload

---
+++ Genai_in_working_model.py
@@ -0,0 +1,14 @@
+import numpy as np
+import google.generativeai as genai 
+from dotenv import load_dotenv
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+load_dotenv()
+
+import os
+key=os.getenv("GOOGLE_API_KEYS")
+genai.configure(api_key=key)
+model=genai.GenerativeModel("gemini-1.5-flash")
+print(model)
+res=model.generate_content(" is the captian of India team")
+print(res.text)
+
